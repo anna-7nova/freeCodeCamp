7b53b2593caa488dc7164245f41ce1ef2ca0a8a5
anna-7nova <anna.7nova@yandex.by>
2024-10-03 00:23:20 +0300

    add a fourth part of task for topic JS

--- 3_JS/1_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator/index_2.js
+++ 3_JS/1_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator/index_2.js
@@ -2,7 +2,7 @@ const character = "#";
 const count = 8;
 const rows = [];
 for (let i = 0; i < count; i = i + 1) {
-  rows.push(character.repeat(i+1));
+  rows.push(character.repeat(i + 1));
 }
 
 let result = "";
@@ -12,9 +12,21 @@ for (const row of rows) {
   result = result + "\n" + row;
 }
 
-function padRow (){
-  
+function padRow(name) {
+  const test = "Testing";
+  console.log("This works!");
+  return test;
+  console.log("This works!");
 }
-padRow();
-const call = padRow();
+
+padRow("Anna");
+const call = padRow("CamperChan");
 console.log(call);
+
+function addTwoNumbers(firstNum, secondNum) {
+  return firstNum + secondNum;
+}
+const sum = addTwoNumbers(5, 10);
+console.log(sum);
+
+
