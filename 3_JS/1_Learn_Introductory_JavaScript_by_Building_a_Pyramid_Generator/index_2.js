d8db29513bcff715871c457bc26b7954c505185d
anna-7nova <anna.7nova@yandex.by>
2024-10-07 21:24:29 +0300

    add a few loops

--- 3_JS/1_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator/index_2.js
+++ 3_JS/1_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator/index_2.js
@@ -3,11 +3,15 @@ const count = 8;
 const rows = [];
 
 function padRow(rowNumber, rowCount) {
-  return " ".repeat(rowCount - rowNumber) + character.repeat(2*rowNumber-1) + " ".repeat(rowCount - rowNumber);
+  return (
+    " ".repeat(rowCount - rowNumber) +
+    character.repeat(2 * rowNumber - 1) +
+    " ".repeat(rowCount - rowNumber)
+  );
 }
-
-for (let i = 0; i < count; i = i + 1) {
-  rows.push(padRow(i+1, count));
+//need to change the code to a different kind of loop.
+for (let i = 1; i < count; i++) {
+  rows.push(padRow(i, count));
 }
 
 let result = "";
@@ -17,12 +21,16 @@ for (const row of rows) {
   result = result + "\n" + row;
 }
 
-
-
 function addTwoNumbers(firstNum, secondNum) {
   return firstNum + secondNum;
 }
 const sum = addTwoNumbers(5, 10);
 console.log(sum);
 
+let done = 0;
+
+while (done!==count) {
+  done++;
+  rows.push(padRow(done, count));
+}
 
